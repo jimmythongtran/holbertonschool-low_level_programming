9689c2409558cfc8586a7d330e1b8124de0312db
Jimmy Tran <jimmy.tran@holbertonschool.com>
2016-10-13 16:09:43 +0000

    Thank you betty-style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,8 @@
 * "Programming is like building a multilingual puzzle
 * Return: Returns 0
 */
-int 	main (void)
+int	main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);	
+	return (0);
 }
