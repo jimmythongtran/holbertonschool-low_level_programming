eb4a977633ec368d5a5386d78f01fe9c513244c4
Jimmy Tran <jimmy.tran@holbertonschool.com>
2016-10-13 16:27:57 +0000

    Formatted

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-/*
-* This main function prints a string
-*
-* Description: This function puts (prints)
-* "Programming is like building a multilingual puzzle
-* Return: Returns 0
-*/
+/**
+ * main - This function prints a string
+ * Description: This function puts (prints to display)
+ * "Programming is like building a multilingual puzzle
+ * Return: Returns 0
+ */
 int	main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
