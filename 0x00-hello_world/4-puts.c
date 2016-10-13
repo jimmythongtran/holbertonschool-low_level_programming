65883278cf36b41bf86b65ca4d4ac7a911a24a9a
Jimmy Tran <jimmy.tran@holbertonschool.com>
2016-10-13 06:44:33 +0000

    Added comments

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
-
+/*
+* This main function prints a string
+*
+* Description: This function puts (prints)
+* "Programming is like building a multilingual puzzle
+* Return: Returns 0
+*/
 int 	main (void)
-
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);	
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-
+/*
+ * This main function prints
+ * a string and returns 0
+ * a newline character added too.
+ * Description: This function prints
+ * "with proper grammer, but the outcome is a piece of art 
+ */
 int	main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
