16734c7d6ad752dd633a25b8b15e1b272b0272ff
Jimmy Tran <jimmy.tran@holbertonschool.com>
2016-10-14 21:40:00 +0000

    Thanks Jennie

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ * except the "e" and "q"
+ * Description: This function uses a while loop to print
+ * lowercase and uppercase alphabet. putchar sends a single
+ * character to the screen in a while loop, but if
+ * the value of a is 'e' or 'q' - it does not putchar to
+ * screen
+ * Return: Returns 0
+ */
+int main(void)
+{
+	int i;
+	int abc;
+
+	i = '0';
+	while (i <= '9')
+	{
+		putchar(i);
+		i++;
+	}
+
+	abc = 'a';
+	while (abc <= 'f')
+	{
+		putchar(abc);
+		abc++;
+	}
+
+	putchar('\n');
+	return (0);
+}
