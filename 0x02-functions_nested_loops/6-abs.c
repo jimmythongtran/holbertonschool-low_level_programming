c0d3e736c1c59c19ae3aab3d5e2f281baa24e88b
Jimmy Tran <jimmy.tran@holbertonschool.com>
2016-10-19 20:23:29 +0000

    Multiplying is key

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+/**
+ * _abs - computes the absolute value
+ * of an integer
+ * @n: the integer we are passing through
+ * Description: using the _abs function
+ * computes the absolute value of an integer
+ * Return: Returns n or 0
+ */
+
+#include "holberton.h"
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else
+	{
+	return (n);
+		}
+
+	return (0);
+}
