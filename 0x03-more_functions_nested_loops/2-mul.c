38d63b69392540fd2ecc85ec87e97499acf222be
Jimmy Tran <jimmy.tran@holbertonschool.com>
2016-10-20 17:11:34 +0000

    First commit

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+/**
+ * mul - multiplies two integers
+ * @a: first operand
+ * @b: second operand
+ * Return: returns product of a and b
+ */
+
+#include "holberton.h"
+
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+	return (product);
+}
---
+++ 0x03-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is the 2nd day of nested loops and functions. It is a 1 day project.
