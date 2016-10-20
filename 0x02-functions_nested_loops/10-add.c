82c2f1c1ccc2e06482930ab296709e1127cbcb40
Jimmy Tran <jimmy.tran@holbertonschool.com>
2016-10-20 03:01:44 +0000

    Always semicolons

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+/**
+ * add - adds two numbers
+ * @a: first operand
+ * @b: second operand
+ * Return: sum of a and b.
+ */
+
+#include "holberton.h"
+
+int add(int a, int b)
+{
+	int t;
+
+	t = a + b;
+
+	return (t);
+}
