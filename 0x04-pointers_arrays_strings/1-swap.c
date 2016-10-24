cd1e77b8e9c54867e17ec864bcb0c7c0e4312eca
Jimmy Tran <jimmy.tran@holbertonschool.com>
2016-10-24 23:15:17 +0000

    Swaparoo

---
+++ 0x04-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+/**
+ * swap_int - swaps the values of two integers
+ * @a: original input
+ * @b: original input
+ * Desription: swaps the value of two integers
+ * using dereferencing
+ * Return: Returns nothing because it is void
+ */
+#include "holberton.h"
+
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = *a;
+}
