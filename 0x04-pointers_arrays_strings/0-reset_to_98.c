23c3c36dac329d73b22988647eee966174d04f02
Jimmy Tran <jimmy.tran@holbertonschool.com>
2016-10-24 23:06:54 +0000

    Betty

--- 0x04-pointers_arrays_strings/0-reset_to_98.c
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -1,3 +1,10 @@
+/**
+ * reset_to_98 - dereferences n to 98
+ * @n: original input
+ * Desription: Using a pointer to change
+ * the value of n
+ * Return: Returns nothing because it is void
+ */
 #include "holberton.h"
 
 void reset_to_98(int *n)
