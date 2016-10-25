c8e2cae75087f810716be0eba3701115909f87d6
Jimmy Tran <jimmy.tran@holbertonschool.com>
2016-10-25 21:32:07 +0000

    Checked

---
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+/**
+ * print_rev - prints a string in reverse
+ * @s: dereferenced operand
+ * Description: prints a string in reverse
+ * Return: Nothing because it's void
+ */
+#include "holberton.h"
+
+void print_rev(char *s)
+{
+	int i;
+	char array[140];
+
+	i = 0;
+	while (*s != 0)
+	{
+		array[i] = *s;
+	i++;
+	s++;
+	}
+	while (i >= 0)
+	{
+		_putchar(array[i]);
+
+	i--;
+	}
+	_putchar ('\n');
+}
