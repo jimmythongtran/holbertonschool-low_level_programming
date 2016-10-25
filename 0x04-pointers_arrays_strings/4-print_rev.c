b65d82a4984d6a711ceb8ec0d113f8523108c1e5
Jimmy Tran <jimmy.tran@holbertonschool.com>
2016-10-25 22:35:39 +0000

    Resubmit

--- 0x04-pointers_arrays_strings/4-print_rev.c
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -9,20 +9,19 @@
 void print_rev(char *s)
 {
 	int i;
-	char array[100];
+	char array[140];
 
 	i = 0;
 	while (*s != 0)
 	{
 		array[i] = *s;
-	i++;
-	s++;
+		i++;
+		s++;
 	}
 	while (i >= 0)
 	{
 		_putchar(array[i]);
-
-	i--;
+		i--;
 	}
 	_putchar ('\n');
 }
