d116eb135a4ea319b308a9196338cea34540ce63
Jimmy Tran <jimmy.tran@holbertonschool.com>
2016-10-26 22:46:51 +0000

    Changed variables

---
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+/**
+ * rev_string - prints a string in reverse
+ * @s: dereferenced operand
+ * Description: prints a string in reverse
+ * Return: Nothing because it's void
+ */
+#include "holberton.h"
+
+void rev_string(char *s)
+{
+	int length;
+	int count;
+	char temp;
+
+	length = 0;
+	while (s[length] != 0)
+	{
+
+	length++;
+	}
+	length--;
+
+	count = 0;
+	while (count < length / 2)
+	{
+		temp = s[length - count];
+		s[length - count] = s[count];
+		s[count] = temp;
+
+	count++;
+	}
+}
