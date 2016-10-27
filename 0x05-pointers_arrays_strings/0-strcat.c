2c0466e9773a21f0d148a57140052c0aaae4f06f
Jimmy Tran <jimmy.tran@holbertonschool.com>
2016-10-27 22:14:41 +0000

    Initial commit

---
+++ 0x05-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,32 @@
+/**
+ * _strcat - concatenates two strings
+ * @dest: one string
+ * @src: one string to append to @dest string
+ * Description: in contatenating the two strings
+ * should overwrite terminating null byte at end of @dest
+ * Return: a pointer to the resulting string @dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int src_i;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	src_i = 0;
+	while (src[src_i] != '\0')
+	{
+		dest[i] = src[src_i];
+		src_i++;
+		i++;
+	}
+
+	i++;
+	dest[i] = '\0';
+	return (dest);
+}
