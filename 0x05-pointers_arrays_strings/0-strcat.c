75b1b2a0cc246351f57e6584983b71f8057005a8
Jimmy Tran <jimmy.tran@holbertonschool.com>
2016-10-27 22:34:16 +0000

    Modified

--- 0x05-pointers_arrays_strings/0-strcat.c
+++ 0x05-pointers_arrays_strings/0-strcat.c
@@ -15,15 +15,17 @@ char *_strcat(char *dest, char *src)
 	i = 0;
 	while (dest[i] != '\0')
 	{
-		i++;
+	
+	i++;
 	}
 
 	src_i = 0;
 	while (src[src_i] != '\0')
 	{
 		dest[i] = src[src_i];
-		src_i++;
-		i++;
+	
+	src_i++;
+	i++;
 	}
 
 	i++;
--- 0x05-pointers_arrays_strings/1-strncat.c
+++ 0x05-pointers_arrays_strings/1-strncat.c
@@ -16,22 +16,17 @@ char *_strncat(char *dest, char *src, int n)
 	i = 0;
 	while (dest[i] != '\0')
 	{
-		i++;
+	
+	i++;
 	}
 
 	src_i = 0;
-	while (src[src_i] != '\0')
+	while (src[src_i] != '\0' && src_i < n)
 	{
 		dest[i] = src[src_i];
-		src_i++;
-		i++;
-	}
-
-	if (src_i < n)
-	{
-		dest[i] = '\0';
-	}
-
+	
 	i++;
+	src_i++;
+	}
 	return (dest);
 }
