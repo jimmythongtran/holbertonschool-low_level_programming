3d934c851f2fbd29d0c95ed5b8f7afa57c529a8f
Jimmy Tran <jimmy.tran@holbertonschool.com>
2016-10-28 19:06:05 +0000

    Ascii is key

---
+++ 0x05-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+char *string_toupper(char *p)
+{
+	int i = 0;
+
+	while (p[i] != '\0')
+	{
+		if (p[i] >= 'a' && p[i] <= 'z')
+		{
+			p[i] -= 32;
+		}
+	i++;
+	}
+	return (p);
+}
