b1b6b769057c1f5cf3e844f37e06d0d07582f270
Jimmy Tran <jimmy.tran@holbertonschool.com>
2016-11-01 00:21:59 +0000

    First commit

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,31 @@
+/**
+ * *_strchr - finding where something appears first
+ * @s: target objective
+ * @c: target
+ * Return: everything after the target
+ */
+
+#include "holberton.h"
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	i++;
+	}
+	if (s[i] == c)
+	{
+		return (s + i);
+	}
+	else
+	{
+		return (0);
+	}
+}
