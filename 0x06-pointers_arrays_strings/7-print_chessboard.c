21c3636606928524a0e3b74b26f1352041072662
Jimmy Tran <jimmy.tran@holbertonschool.com>
2016-11-01 04:39:22 +0000

    First commit

---
+++ 0x06-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+/**
+ * print_chessboard - prints the chessboard
+ * @a: the board to print to display
+ * Return: Nothing
+ */
+
+#include "holberton.h"
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	i = 0;
+	while (i < 8)
+	{
+		j = 0;
+		while (j < 8)
+		{
+			_putchar(a[i][j]);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
