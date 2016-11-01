b4004d22df31c149a2dd97397fd56ee392fea16c
Jimmy Tran <jimmy.tran@holbertonschool.com>
2016-11-01 23:26:56 +0000

    First commit

---
+++ 0x06-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * *set_string - dereferences s to to
+ * @s: (double) pointer variable
+ * @to: pointer variable
+ * Return: Nothing
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
+
