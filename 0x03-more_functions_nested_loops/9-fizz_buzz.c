fe08ed339c71ff627cc49cae6102d85ff59da03b
Jimmy Tran <jimmy.tran@holbertonschool.com>
2016-11-07 21:47:34 +0000

    First commit

---
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+/**
+ * main - fizz buzz
+ * Return: Returns 0
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+
+	i = 1;
+	while (i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		if (i != 100)
+		{
+			printf(" ");
+		}
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
