e2144dcb7da0ac70cd7331ce1bc05d827fc2dee9
Jimmy Tran <jimmy.tran@holbertonschool.com>
2016-11-10 00:35:46 +0000

    Rework

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -1,38 +1,56 @@
+#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
+ * str_len - counts how many characters are in the string
+ * @s: pointer variable
+ * Return: number of characters in the string
+ */
+
+int str_len(char *s)
+{
+	int count;
+
+	count = 0;
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+	return (count);
+}
+
+/**
  * str_concat - concatenates two strings
- * @s1: string 1
- * @s2: string 2 to concatenate onto string 1
- * Return: Returned pointer points to a newly allocated space in memory
- * which contains s1 followed by s2 and null terminated
- * If function fails, returns null
+ * @s1: string one
+ * @s2: string two to concatenate to string one
+ * Return: Returns the pointer or NULL if fails
  */
 
 char *str_concat(char *s1, char *s2)
 {
-	int i, j;
-	char *concat;
+	int count, count2;
+	char *a;
 
-	concat = malloc(sizeof(*s1 + *s2));
-
-	i = 0;
-	while (s1[i] != '\0')
+	a = malloc(str_len(s1) + str_len(s2) + 1);
+	if (a == NULL)
 	{
-		concat[i] = s1[i];
-	i++;
+		return (NULL);
 	}
-
-	j = 0;
-	while (s2[j] != '\0')
+	count = 0;
+	while (count <= str_len(s1))
 	{
-		concat[i] = s2[j];
-	j++;
-	i++;
+		a[count] = s1[count];
+		count++;
 	}
-
-	concat[i] = s2[j];
-
-	return (concat);
+	count--;
+	count2 = 0;
+	while (count2 <= str_len(s2))
+	{
+		a[count] = s2[count2];
+		count++;
+		count2++;
+	}
+	a[count] = '\0';
+	return (a);
 }
