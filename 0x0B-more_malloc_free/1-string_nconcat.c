6e7e78d9900774fae912fd20ae0b95b282203871
Jimmy Tran <jimmy.tran@holbertonschool.com>
2016-11-11 00:43:35 +0000

    First commit

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -1,10 +1,60 @@
 #include <stdlib.h>
-/**
-  * name - description
-  * @var - variable and what it does
-  * Return: return 0 on success
-  */
-int main(void)
-{
-	return (0);
 
+/**
+ * _strlen - counts how many characters are in a string
+ * @str: string to count
+ * Return: number of characters in the string
+ */
+int _strlen(char *str)
+{
+	int length = 0;
+
+	while (str[length] != '\0')
+		length++;
+	return (length);
+}
+
+/**
+ * string_nconcat - concats two strings up to the n number of bytes
+ * @s1: first string to concat. Whole string gets added
+ * @s2: second string to concat. up to the Nth byte gets appended
+ * @n: number of bytes of second string to concat
+ * Return: pointer to beginning of string
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *new_string;
+	unsigned int combined_length, i, j, s1length, s2length;
+
+	if (s1 == NULL)
+		s1length = 0;
+	else
+		s1length = _strlen(s1);
+	if (s2 == NULL)
+		s2length = 0;
+	else
+		s2length = _strlen(s2);
+	if (s2length <= n)
+		combined_length = s1length + s2length;
+	else
+		combined_length = s1length + n;
+
+	new_string = malloc(sizeof(char) * combined_length);
+	if (new_string == NULL)
+		return (NULL);
+
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		new_string[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while ((i + j) < combined_length)
+	{
+		new_string[i + j] = s2[j];
+		j++;
+	}
+	return (new_string);
+}
