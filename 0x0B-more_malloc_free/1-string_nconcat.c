9a48dcc449b30f1943e51c8962c34e45ab6d26b5
Jimmy Tran <jimmy.tran@holbertonschool.com>
2016-11-12 18:55:51 +0000

    Reorganized syntax

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -14,24 +14,28 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, len, j;
 	unsigned int size;
 
-	len = 0;
 	empty = "";
 	if (s1 == NULL)
 		s1 = empty;
 	if (s2 == NULL)
 		s2 = empty;
+	
+	len = 0;
 	while (s1[len] != '\0')
 		len++;
+	
 	size = (len + n) * sizeof(*nstr);
-	nstr = malloc(size + 1);
+	nstr = malloc(size + 1);	
 	if (nstr == NULL)
 		return (NULL);
+
 	i = 0;
 	while (i < size && s1[i] != '\0')
 	{
 		nstr[i] = s1[i];
 		i++;
 	}
+	
 	j = 0;
 	while (i < size && s2[j] != '\0')
 	{
@@ -39,6 +43,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		i++;
 		j++;
 	}
+	
 	nstr[i] = '\0';
 	return (nstr);
 }
