98e3f712e4238ff2dc7c1e87b8216c492069553f
Jimmy Tran <jimmy.tran@holbertonschool.com>
2016-11-14 18:08:37 +0000

    First commit

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - Short description
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
