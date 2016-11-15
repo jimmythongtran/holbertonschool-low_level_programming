438b8630fff183807ed749982815cb0a94084aa5
Jimmy Tran <jimmy.tran@holbertonschool.com>
2016-11-15 19:40:03 +0000

    putchar

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef DOG
 #define DOG
+int _putchar(char c);
 /**
  * struct dog - Short description
  * @name: name of dog
@@ -15,10 +16,15 @@ struct dog
 	char *owner;
 };
 
+/* Ex 1 */
 void init_dog(struct dog *d, char *name, float age, char *owner);
+/* Ex 2 */
 void print_dog(struct dog *d);
+/* Ex 3 */
 typedef struct dog dog_t;
+/* Ex 4 */
 dog_t *new_dog(char *name, float age, char *owner);
+/* Ex 5 */
 void free_dog(dog_t *d);
 
 #endif
