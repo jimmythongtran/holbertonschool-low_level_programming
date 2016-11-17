c5514039a1bd8203aca941eb12ea38eebca3cb2f
Jimmy Tran <jimmy.tran@holbertonschool.com>
2016-11-17 07:20:35 +0000

    First commit

--- 0x0E-function_pointers/0-print_name.c
+++ 0x0E-function_pointers/0-print_name.c
@@ -1,12 +1,16 @@
 /**
  * print_name - a function that prints a name
  * using pointers to functions
- * @name: 
- *
+ * @name: name
+ * @f: function to print name
+ * Return: nothing because void
  */
-#include "function_pointers.h"
+#include <stdio.h>
 
 void print_name(char *name, void (*f)(char *))
 {
+	void (*pfn)(char *);
 
+	pfn = f;
+	pfn(name);
 }
