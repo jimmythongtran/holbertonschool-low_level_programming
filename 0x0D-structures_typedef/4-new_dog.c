cf93552dd8383c5fccbbb322e7f928dd0d2e7585
Jimmy Tran <jimmy.tran@holbertonschool.com>
2016-11-17 07:15:22 +0000

    First commit

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -1,5 +1,43 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <stdio.h>
+
+
+/**
+ * _strdup - returns a pointer to a newly allocated
+ * space in memory, containing a copy of the string given as a parameter
+ * @str: string
+ * Return: copy string
+ */
+
+char *_strdup(char *str)
+{
+	char *copy_string;
+	unsigned int i, j;
+
+	if (str == NULL)
+		return (NULL);
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	i++;
+
+	copy_string = malloc(i * sizeof(char));
+	if (copy_string == NULL)
+		return (NULL);
+
+	j = 0;
+	while (str[j] != '\0')
+	{
+		copy_string[j] = str[j];
+		j++;
+	}
+	return (copy_string);
+	
+
 /**
  * new_dog - a function that creates a new dog
  * @name: dog name
@@ -14,8 +52,19 @@ dog_t *new_dog(char *name, float age, char *owner)
 	tmp = malloc(sizeof(dog_t));
 	if (tmp == NULL)
 		return (NULL);
-	tmp->name = name;
+	tmp->name = _strdup(name);
+	if (tmp->name == NULL)
+	{
+	  free(tmp);
+	  return (NULL);
+	}
 	tmp->age = age;
-	tmp->owner = owner;
+	tmp->owner = _strdup(owner);
+	if (tmp->owner == NULL)
+	{
+	  free(tmp->owner);
+	  free(tmp)
+	  return (NULL);
+	}
 	return (tmp);
 }
---
+++ 0x0E-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+/**
+ * print_name - a function that prints a name
+ * using pointers to functions
+ * @name: 
+ *
+ */
+#include "function_pointers.h"
+
+void print_name(char *name, void (*f)(char *))
+{
+
+}
