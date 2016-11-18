163103f318085c9be33994d4fd174f7fea6ee0f0
Jimmy Tran <jimmy.tran@holbertonschool.com>
2016-11-18 06:23:19 +0000

    Betty

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -9,32 +9,32 @@
  * Return: 0
  */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int a, b;
 	int (*o)(int, int);
-	
+
 	if (argc != 4)
 	{
 		printf("Error\n");
-		exit (98);
+		exit(98);
 	}
-	
+
 	if (argv[2][1] != '\0')
 	{
 		printf("Error\n");
-		exit (99);
+		exit(99);
 	}
 
-	o = get_op_func (argv[2]);
+	o = get_op_func(argv[2]);
 	if (o == NULL)
 	{
 		printf("Error\n");
-		exit (99);
+		exit(99);
 	}
 
-	a = atoi (argv[1]);
-	b = atoi (argv[3]);
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
 
 	printf("%d\n", o(a, b));
 
