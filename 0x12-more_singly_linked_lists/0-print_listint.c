6a1b6626a2b4b4e2c88fcdcc6b3d4638f8a29e2c
Jimmy Tran <jimmy.tran@holbertonschool.com>
2016-11-30 19:14:28 +0000

    Change 1 data type

---
+++ 0x12-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * print_listint - prints a list_t list
+ * @h: pointer to struct
+ * Return: counter
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t  counter;
+
+	counter = 0;
+	while (h != NULL)
+	{
+/* Used template from yesterday, changed the data type to one instead of two */
+		printf("%d\n", h->n);
+		h = h->next;
+		counter++;
+	}
+	return (counter);
+}
