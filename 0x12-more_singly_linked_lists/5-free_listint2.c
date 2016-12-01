4f904e99b27c089d6ad36c030f837abe532af31f
Jimmy Tran <jimmy.tran@holbertonschool.com>
2016-12-01 07:05:40 +0000

    Double pointer

---
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_listint2 - frees the memory of listint_t
+ * @head: a pointer to the head of the linked list
+ */
+
+/* pass a double pointer instead of a pointer */
+/* dereference the pointer to use its value like single pointer */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+/* checks if pointer to the head is NULL, if the list is empty, it returns */
+	while (*head != NULL)
+	{
+/* save the pointer to the head in a temp variable */
+		temp = *head;
+/* make pointer to the head point to the next node on list */
+		*head = (*head)->next;
+/* safely free(temp) variable - head points to the rest of the list */
+		free(temp);
+	}
+}
