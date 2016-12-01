c26352ed298fbdf3e3f03500e1214093611e83ae
Jimmy Tran <jimmy.tran@holbertonschool.com>
2016-12-01 01:38:48 +0000

    Add detailed comments

--- 0x11-singly_linked_lists/4-free_list.c
+++ 0x11-singly_linked_lists/4-free_list.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "lists.h"
 /**
  * free_list - frees the memory of list_t
@@ -5,13 +6,16 @@
  */
 void free_list(list_t *head)
 {
-/* when head reaches the end */
-	if (head == NULL)
+	list_t *temp;
+/* checks if head is NULL, if the list is empty, it returns */
+	while (head == NULL)
 	{
-		return;
+/* save the head in a temp variable */
+		temp = head;
+/* make head point to the next node on the list */
+		head = head->next;
+/* safely free(temp->str) and free(temp) variable - head points to rest of the list */
+		free(temp->str);
+		free(temp);
 	}
-/* free all memory allocations */
-	free_list(head->next);
-	free(head->str);
-	free(head);
 }
