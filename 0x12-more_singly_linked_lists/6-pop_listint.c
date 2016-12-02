a1fd4698c11d9e0bc86fe42f5c411028048c2e72
Jimmy Tran <jimmy.tran@holbertonschool.com>
2016-12-02 00:27:29 +0000

    First commit

---
+++ 0x12-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * pop_listint - deletes the head node of a listint_t
+ * returns the head node's data (n)
+ * @head: head node
+ * Return: head node's data
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int n; /* holds headNodeData */
+
+/* checks if the pointer of head is NULL */
+	if (*head == NULL)
+		return (0);
+
+/* temp holds the address of head */
+	temp = *head;
+
+/* take the next item that the head points to and save it */
+	n = temp->n;
+
+/* points to the first node */
+	*head = temp->next;
+
+/* pops the head node*/
+	free(temp);
+
+/* returns head node's data */
+	return (n);
+}
