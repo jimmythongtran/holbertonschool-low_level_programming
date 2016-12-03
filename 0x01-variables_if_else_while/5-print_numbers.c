79bc9ed947e0bb13facd4ea6c547bef030111cba
Jimmy Tran <jimmy.tran@holbertonschool.com>
2016-12-03 18:41:48 +0000

    Update description

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - prints the alphabet in lowercase
- * except the "e" and "q"
- * Description: This function uses a while loop to print
- * lowercase and uppercase alphabet. putchar sends a single
- * character to the screen in a while loop, but if
- * the value of a is 'e' or 'q' - it does not putchar to
- * screen
+ * main - prints all the numbers of base 10, starting
+ * from 0, followed by a new line
+ * Description: uses printf
  * Return: Returns 0
  */
 int main(void)
