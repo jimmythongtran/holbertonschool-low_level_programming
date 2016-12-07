bd11f87eaa6b7047ab7266819349798143aba78f
Jimmy Tran <jimmy.tran@holbertonschool.com>
2016-12-07 18:56:11 +0000

    Initial commit

---
+++ 0x14-file_io/0-read_textfile.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * read_textfile - reads a text file and prints it to
+ * the POSIX standard output
+ * @filename: pointer to the filename
+ * @letters: the number of letters it should read and print
+ * Return: Returns the actual number of letters it could read and print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+}
