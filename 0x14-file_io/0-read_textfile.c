16092281ce3ed61394f03a7e5af3d8eefe56b297
Jimmy Tran <jimmy.tran@holbertonschool.com>
2016-12-07 22:10:50 +0000

    Bits and pieces

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -8,7 +8,33 @@
  * Return: Returns the actual number of letters it could read and print
  */
 
+/*we want to output the Oscar file and printed chars [468]*/
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	FILE *file;
+	
+	char c;
 
+/*spec*/
+	if (filename == NULL)
+		return (0);
+
+/* 1st para: path of the file */
+/* 2nd para: open mode, what do you plan on doing with your file? */
+/* returns a file struct pointer */
+	file = open(filename, "O_RDONLY");
+
+/*fgetc returns the EOF constant, and the file constant whenever its finished reading*/
+/*EOF is non-printable character*/
+	while ((c = fgetc(file)) != EOF);
+	{
+		printf("%c", c);
+	}
+
+/*clean up*/
+	fclose(file);
+
+/* return the actual number of letters*/
+	return (0);
 }
--- 0x14-file_io/holberton.h
+++ 0x14-file_io/holberton.h
@@ -1,5 +1,11 @@
 #ifndef FILE_IO
 #define FILE_IO
+#include <unistd.h> /*for IO functions*/
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
