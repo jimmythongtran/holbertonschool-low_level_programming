d30e2278756857d0af8347af7bff05c5589b4a39
Jimmy Tran <jimmy.tran@holbertonschool.com>
2016-12-08 01:39:27 +0000

    Make TODOs

---
+++ 0x14-file_io/1-create_file.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * create_file - creates a file
+ * @filename: name of the file to create
+ * @text_content: a NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure
+ */
+
+/*
+ * TODO: created file must have permissions: rw-------
+ * TODO: if filename is NULL return -1
+ * TODO: if text_content is NULL create an empty file
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+
+}
