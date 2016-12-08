e9e99b36f58da1792eda9c0171fe69ab880b949c
Jimmy Tran <jimmy.tran@holbertonschool.com>
2016-12-08 23:06:45 +0000

    changed write fd

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -37,7 +37,7 @@ int create_file(const char *filename, char *text_content)
 		i++;
 	}
 
-	check = write(STDIN_FILENO, text_content, i);
+	check = write(fd, text_content, i);
 	if (check == -1)
 		return (-1);
 
