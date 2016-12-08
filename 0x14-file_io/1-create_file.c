80d15ceda0730a16eb1da76b2f98774a45605852
Jimmy Tran <jimmy.tran@holbertonschool.com>
2016-12-08 02:50:10 +0000

    Add permissions

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -7,20 +7,24 @@
  * Return: 1 on success, -1 on failure
  */
 
-/*
- * TODO: created file must have permissions: rw-------
- */
-
 int create_file(const char *filename, char *text_content)
 {
 
 /*declare variables*/
+	int inputFile;
 
+/*specs*/
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content == NULL)
 		/* create an empty file*/
 
-	
+/*opens file with permissions: rw------- */
+	inputFile = open(filename, O_RDWR | O_CREAT, 0600);
+
+
+/*clean up*/
+	close(inputFile);
+	return(1);
 }
