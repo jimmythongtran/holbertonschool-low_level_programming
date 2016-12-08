abcf0fee86d7222417f6db8af265630417b8636e
Jimmy Tran <jimmy.tran@holbertonschool.com>
2016-12-08 23:20:43 +0000

    Change number permissions

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 /*opens file with permissions: rw------- */
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IREAD | S_IWRITE);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 	{
 		return (-1);
@@ -37,7 +37,8 @@ int create_file(const char *filename, char *text_content)
 		i++;
 	}
 
-	check = write(STDOUT_FILENO, text_content, i);
+	check = write(fd, text_content, i);
+	
 	if (check == -1)
 		return (-1);
 
