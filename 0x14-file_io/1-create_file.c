252ec1db1631750a359dad616bf3be9034644cfa
Jimmy Tran <jimmy.tran@holbertonschool.com>
2016-12-08 03:01:42 +0000

    Update permissions

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -22,7 +22,8 @@ int create_file(const char *filename, char *text_content)
 
 /*opens file with permissions: rw------- */
 	inputFile = open(filename, O_RDWR | O_CREAT, 0600);
-
+	if (inputFile == -1)
+		return (0);
 
 /*clean up*/
 	close(inputFile);
