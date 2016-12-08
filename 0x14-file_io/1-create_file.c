bee0db034bfea8b8588192507c58266857f4f19a
Jimmy Tran <jimmy.tran@holbertonschool.com>
2016-12-08 23:31:51 +0000

    Add check

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -28,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content == NULL)
 	{
-		return (-1);
+		return (1);
 	}
 
 	i = 0;
@@ -36,8 +36,8 @@ int create_file(const char *filename, char *text_content)
 	{
 		i++;
 	}
-
-	check = write(fd, text_content, i);
+	if (i > 0)
+		check = write(fd, text_content, i);
 
 	if (check == -1)
 		return (-1);
