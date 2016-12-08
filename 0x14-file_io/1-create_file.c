15e4d9faeedbe1d9f81c1fd098215d593377c00b
Jimmy Tran <jimmy.tran@holbertonschool.com>
2016-12-08 01:58:51 +0000

    Update TODOs

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -9,11 +9,18 @@
 
 /*
  * TODO: created file must have permissions: rw-------
- * TODO: if filename is NULL return -1
- * TODO: if text_content is NULL create an empty file
  */
 
 int create_file(const char *filename, char *text_content)
 {
 
+/*declare variables*/
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+		/* create an empty file*/
+
+	
 }
