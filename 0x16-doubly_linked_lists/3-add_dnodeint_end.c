2a63ea856764d4ad1c25a8c9dc481a42b6b53e47
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-08 23:06:04 +0000

    Add ->prev

--- 0x16-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x16-doubly_linked_lists/3-add_dnodeint_end.c
@@ -5,22 +5,24 @@
 
 /**
  * add_dnodeint_end - adds a new node at the end of a list_t list
- * @head: linked list
- * @n: integer
- * Return: temp (placeholder)
+ * @head: head of the doubly linked list
+ * @n: integer value for new node
+ * Return: address of element or NULL
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	listint_t *newNode;
-	listint_t *temp;
+	dlistint_t *newNode;
+	dlistint_t *temp;
 
 /* create space in memory for newNode */
-	newNode = malloc(sizeof(listint_t));
+	newNode = malloc(sizeof(dlistint_t));
 	if (newNode == NULL)
 		return (NULL);
 
 /* Places the values into nodes */
 	newNode->n = n;
+/* This is all it took because it needs to traverse */
+	newNode->prev = NULL;
 	newNode->next = NULL;
 	temp = *head;
 	if (*head == NULL)
