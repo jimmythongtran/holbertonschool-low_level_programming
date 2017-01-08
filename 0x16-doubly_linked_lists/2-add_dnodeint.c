73e6a5910e2b767421f713144cd172cb56024698
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-08 22:39:49 +0000

    Moved next to head

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -27,6 +27,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	newNode->n = n;
 	newNode->prev = NULL;
+/* excluding next returns last value 1024 only*/
 	newNode->next = *head;
 	*head = newNode;
 
