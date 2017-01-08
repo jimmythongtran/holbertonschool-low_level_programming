1f7642190f6fa1edc0fdf54ca933dfc4bb68f246
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-08 18:40:52 +0000

    AMend comments

--- 0x16-doubly_linked_lists/1-dlistint_len.c
+++ 0x16-doubly_linked_lists/1-dlistint_len.c
@@ -3,9 +3,9 @@
 #include <stdlib.h>
 
 /**
- * dlistint_len - prints a list_t list
- * @h: pointer to struct
- * Return: counter
+ * dlistint_len - prints the number of elements in dlist_t list
+ * @h: head pointer of doubly linked list
+ * Return: number of elements
  */
 /* Only change is name of function prototype */
 size_t dlistint_len(const dlistint_t *h)
