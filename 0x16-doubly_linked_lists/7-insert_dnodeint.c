595256ff5b5d15f3e412d617b622de8a5738918d
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-09 22:51:05 +0000

    Changed prototype in lists.h

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -8,28 +8,26 @@
  *@head: double pointer to head
  *@index: the index of the list where new node should be added
  *@n: integer
- * Return: Returns new node data
+ * Return: Returns new node address
  */
 
-dlistint_t *insert_dnodeint_at_index(dlistint_t **head, unsigned int index, int n)
+dlistint_t *insert_dnodeint_at_index(dlistint_t **head, unsigned int idx, int n)
 {
 	dlistint_t *new_node, *temp; /* */
 	unsigned int i; /*counter*/
 
 /* edge case for NULL*/
-	if (*head == NULL && index != 0)
+	if (*head == NULL && idx != 0)
 		return (NULL);
 /*creating a new node by freeing space in memory*/
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n; /* filling out the new node with data */
-	new_node->prev = NULL; /*TODO: added*/
 /* edge case for 0 */
-	if (index == 0)
+	if (idx == 0)
 	{
 		new_node->next = *head;
-		*head->prev = new_node;/*TODO: added*/
 		*head = new_node;
 		return (new_node);
 	}
@@ -37,21 +35,19 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **head, unsigned int index, int 
 /* index-1 because we want to find the node before the one we insert */
 	i = 0;
 	temp = *head;
-	while (i < (index - 1) && temp != NULL)
+	while (i < (idx - 1) && temp != NULL)
 	{
 		i++;
 		temp = temp->next;
 	}
 /* the original linked list went out of bounds */
-	if (i != (index - 1))
+	if (i != (idx - 1))
 	{
 		free(new_node);
 		return (NULL);
 	}
 /* link previous and following node to newly created node */
-	new_node->prev = temp;/*TODO: added*/
 	new_node->next = temp->next;
-	temp->prev = new_node;
 	temp->next = new_node;
 	return (new_node);
 }
