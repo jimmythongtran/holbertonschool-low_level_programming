8bff4e04d0ec2be0c7b964a727eeef36cc68aa07
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-02-02 23:52:52 +0000

    Add templates

---
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -0,0 +1,11 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: the hash table to add or update the key/value to
+ * @key: key
+ * @value: value associated with key
+ * Return: 1 if successful
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+{
+}
