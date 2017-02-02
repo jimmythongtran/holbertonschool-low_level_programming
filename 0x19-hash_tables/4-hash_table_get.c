1b76968870ec9e620d52bddbd9dcb7164a117a5e
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-02-02 23:56:05 +0000

    Added templates

---
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -0,0 +1,10 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - retrives a value associated with a key
+ * @ht: hash table you want to look into
+ * @key: key to look for
+ * Return: value associated with element or NULL if key can't be found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+}
