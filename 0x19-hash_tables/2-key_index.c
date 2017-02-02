26a69a25ba025459ebc3de1cf1e49e6e5cea4385
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-02-02 23:41:08 +0000

    2's template

---
+++ 0x19-hash_tables/2-key_index.c
@@ -0,0 +1,10 @@
+#include "hash_tables.h"
+/**
+ * key_index - gives you the index of a key
+ * @key: key to find its index
+ * @size: size of array for hash table
+ * Return: index
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+}
