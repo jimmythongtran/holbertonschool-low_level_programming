48d398db2039eecd1459311bed74efeed58b3519
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-02-02 23:26:10 +0000

    Dan Bernstein's a champ

--- 0x19-hash_tables/1-djb2.c
+++ 0x19-hash_tables/1-djb2.c
@@ -6,10 +6,13 @@
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-	unsigned long hash = 5381;
+	unsigned long int hash;
 	int c;
-
-	while (c = *str++)
-	hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
 	return (hash);
 }
