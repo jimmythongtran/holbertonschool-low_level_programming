aa2050e09d5f9a18681926b71635360d289153ea
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-02-04 19:30:14 +0000

    Seperated add node to be clearer

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -1,4 +1,20 @@
 #include "hash_tables.h"
+
+hash_node_t *create_node(const char *key, const char *value)
+{
+		hash_node_t *newPair;
+
+		if (key == NULL || value == NULL)
+				return (0);
+		newPair = malloc(sizeof(hash_node_t));
+		if (newPair == NULL)
+				return (0);
+		/*calculates the space needed and allocates it and copies the source string into the newly allocated space*/
+		newPair->key = (strdup)key;
+		newPair->value = (strdup)value;
+		newPair->next = NULL;
+		return (new_node)
+}
 /**
  * hash_table_set - adds an element to the hash table
  * @ht: the hash table to add or update the key/value to
@@ -16,7 +32,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 		if (key == NULL)
 				return (0);
 
-		index = key_index(const unsigned char *)key, ht->size);
+		index = key_index((const unsigned char *)key, ht->size);
 		if (ht->array[index])
 		{
 				temp = ht->array[index];
@@ -31,15 +47,5 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 						temp = tmp->next;
 				}
 				return (1);
-		}	
-/*add node to hash table*/
-		newPair = malloc(sizeof(hash_node_t));
-		if (newPair == NULL)
-				return (0);
-		/*calculates the space needed and allocates it and copies the source string into the newly allocated space*/
-		newPair->key = (strdup)key;
-		newPair->value = (strdup)value;
-		newPair->next = NULL;
-
-
+		}
 }
