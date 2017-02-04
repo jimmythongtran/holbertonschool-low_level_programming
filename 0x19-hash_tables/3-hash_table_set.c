62b401af26b75ddb2ddeca99b372bee64befd83a
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-02-04 01:38:54 +0000

    newPair made, working index

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -8,4 +8,38 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 {
+		unsigned long int index;
+		hash_node_t *newPair, *temp;
+
+		if (ht == NULL)
+				return (0);
+		if (key == NULL)
+				return (0);
+
+		index = key_index(const unsigned char *)key, ht->size);
+		if (ht->array[index])
+		{
+				temp = ht->array[index];
+				while (temp)
+				{
+						/*two strings equal only if they have same characters in same order*/
+						if (strcmp(temp->key, key))
+						{
+								temp->key = strdup(key);
+								break;
+						}
+						temp = tmp->next;
+				}
+				return (1);
+		}	
+/*add node to hash table*/
+		newPair = malloc(sizeof(hash_node_t));
+		if (newPair == NULL)
+				return (0);
+		/*calculates the space needed and allocates it and copies the source string into the newly allocated space*/
+		newPair->key = (strdup)key;
+		newPair->value = (strdup)value;
+		newPair->next = NULL;
+
+
 }
