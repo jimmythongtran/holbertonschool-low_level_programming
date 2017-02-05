77f11702fc0ad7c19463c0ae64a19b0eb06e0e0a
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-02-05 23:41:30 +0000

    Seg faults

--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -7,4 +7,21 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	hash_node_t *temp;
+	unsigned long int index;
+	
+	index = key_index((const unsigned char *)key, ht->size);
+	temp = ht->array[index];
+
+	printf("Check1");
+
+	if (temp == NULL)
+		return (NULL);
+	else
+		return (temp->value);
+
+	printf("Check2");
+
+	while (temp->next !=  NULL)
+		temp = temp->next;
 }
