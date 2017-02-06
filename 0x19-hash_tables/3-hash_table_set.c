568bfc29d56195bc3223905d97b0833773c1aa41
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-02-06 00:13:11 +0000

    No valgrind and print OK

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -11,32 +11,25 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *newPair, *temp;
 
-	if (ht == NULL)
+	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
-	if (key == NULL)
-		return (0);
-
-	printf("Check1");
 
 	index = key_index((const unsigned char *)key, ht->size);
 	/*pointer to hash_node_t - beg of linked list*/
 	temp = ht->array[index];
 	/*making a node that stores the key values*/
-	printf("Check2");
-
-	if (key == NULL || value == NULL)
-		return (0);
+	/*malloc for new node pair*/
 	newPair = malloc(sizeof(hash_node_t));
 	if (newPair == NULL)
 		return (0);
+	/*assign new node values*/
 	newPair->key = strdup(key);
 	if (newPair->key == NULL)
 		return (0);
 	newPair->value = strdup(value);
 	if (newPair->value == NULL)
 		return (0);
-	newPair->next = NULL;
-	*temp = *newPair;
+	newPair->next = temp;
 
 	return (1);
 }
