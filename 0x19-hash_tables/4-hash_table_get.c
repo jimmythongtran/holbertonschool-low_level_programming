18a1448638eebbff9618ad49b3e2af8eccccae1d
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-02-06 00:51:56 +0000

    Prints first param

--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -9,18 +9,17 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	hash_node_t *temp;
 	unsigned long int index;
-	
+
+	if (ht == NULL || key == NULL)
+		return (0);
+	/*get index for value*/
 	index = key_index((const unsigned char *)key, ht->size);
+	/*head of linked list array*/
 	temp = ht->array[index];
-
-	printf("Check1");
-
 	if (temp == NULL)
 		return (NULL);
 	else
 		return (temp->value);
-
-	printf("Check2");
 
 	while (temp->next !=  NULL)
 		temp = temp->next;
