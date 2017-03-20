7dc754667b276d605f6024e3357cef8869331c94
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-03-20 04:56:02 +0000

    Added comments

--- 0x1C-binary_trees/5-binary_tree_is_root.c
+++ 0x1C-binary_trees/5-binary_tree_is_root.c
@@ -1,7 +1,8 @@
 #include "binary_trees.h"
 /**
- *
- *
+ * binary_tree_is_root - checks if node is root
+ * @node: node to check
+ * Return: 1 if root, 0 if not
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
