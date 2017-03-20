ca65059c3d5dbc6d205fff5149a3a65199ed95f9
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-03-20 18:22:19 +0000

    return 0

--- 0x1C-binary_trees/4-binary_tree_is_leaf.c
+++ 0x1C-binary_trees/4-binary_tree_is_leaf.c
@@ -6,7 +6,11 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node == NULL || (node->right == NULL && node->left == NULL))
+	if (node == NULL)
+	{
+		return (0);
+	}
+	if (node->right == NULL && node->left == NULL)
 	{
 		return (1);
 	}
