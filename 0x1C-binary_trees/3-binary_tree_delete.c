64e43ccb949fe2b1c5b9f0b2f61fd33bdcecc37a
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-03-20 05:18:02 +0000

    betty

--- 0x1C-binary_trees/3-binary_tree_delete.c
+++ 0x1C-binary_trees/3-binary_tree_delete.c
@@ -1,8 +1,7 @@
 #include "binary_trees.h"
 /**
  * binary_tree_delete - deletes an entire binary tree
- * @parent: parent node of node to create
- * @value: value to insert
+ * @tree: binary tree
  * Return: NULL if fail, or new node parent
  */
 void binary_tree_delete(binary_tree_t *tree)
