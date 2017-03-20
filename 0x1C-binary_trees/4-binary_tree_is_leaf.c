8baa902c116920336816d01678f99e6698b42fe1
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-03-20 18:16:10 +0000

    Check node for null

--- 0x1C-binary_trees/4-binary_tree_is_leaf.c
+++ 0x1C-binary_trees/4-binary_tree_is_leaf.c
@@ -6,7 +6,7 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node->right == NULL && node->left == NULL)
+	if (node == NULL || (node->right == NULL && node->left == NULL))
 	{
 		return (1);
 	}
