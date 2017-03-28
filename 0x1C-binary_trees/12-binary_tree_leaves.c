04739fc5ce50d3125e736f9c962c1fc3030c1cf9
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-03-28 02:04:28 +0000

    changed to geek, betty

--- 0x1C-binary_trees/12-binary_tree_leaves.c
+++ 0x1C-binary_trees/12-binary_tree_leaves.c
@@ -7,17 +7,10 @@
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	int leaves;
-
-	leaves = 0;
-
-	if (tree->left == NULL && tree->right == NULL)
-		return (1);
 	if (tree == NULL)
 		return (0);
-	if (tree->left)
-		leaves += 1;
-	if (tree->right)
-		leaves += 1;
-	return (leaves);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	return (binary_tree_leaves(tree->left)
+			+ binary_tree_leaves(tree->right));
 }
