7417d445a64b2f03be5250c5fcca9533fdbb3ac7
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-04-13 21:40:03 +0000

    1st draft header

---
+++ 0x1D-search_algorithms/search_algos.h
@@ -0,0 +1,13 @@
+#inndef SEARCH_ALGOS
+#define SEARCH_ALGOS
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Task 0 */
+int linear_search(int *array, size_t size, int value);
+
+/* Task 1*/
+int binary_search(int *array, size_t size, int value);
+
+#endif
