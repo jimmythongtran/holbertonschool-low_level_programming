1c309c9b95bfa5f56212ca4b077633fd01844191
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-04-13 23:53:19 +0000

    %zu for size_t, not %lu

--- 0x1D-search_algorithms/0-linear.c
+++ 0x1D-search_algorithms/0-linear.c
@@ -4,13 +4,19 @@
  * @array: pointer to 1st elem of array to search in
  * @size: number of elements in array
  * @value: value to search for
+ * Return: value searched for, otherwise -1
  */
 
 int linear_search(int *array, size_t size, int value)
 {
-	unsigned int i;
-	for (i = 0; i<size;i++)
+	size_t i;
+
+	if (array == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
 	{
+		printf("Value checked at array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
 	}
